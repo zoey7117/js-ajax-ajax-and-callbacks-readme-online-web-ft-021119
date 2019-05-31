dc922966590c7cf1f8aeda412ab60455c13992ef
github username <github email address>
2019-05-31 13:34:10 -0400

    Done.

---
+++ script.js
@@ -0,0 +1,17 @@
+// We should wait for the page to load before running our Ajax request
+$(document).ready(function() {
+	// Now we start the Ajax GET request. The first parameter is the URL with the data.
+	// The second parameter is a function that handles the response.
+	$.get('sentence.html', function(response) {
+		// Here we are getting the element on the page with the id of sentences and
+		// inserting the response
+		$('#sentences').html(response);
+	});
+  $.get('this_doesnt_exist.html', function(data) {
+    // This will not be called because the .html file request doesn't exist
+    doSomethingGood();
+}).fail(function(error) {
+    // This is called when an error occurs
+    console.log('Something went wrong: ' + error.statusText);
+});
+});
